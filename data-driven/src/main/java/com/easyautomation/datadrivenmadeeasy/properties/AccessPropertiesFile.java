ecf31aec5be172c4a6979e903faee4a480c06e67
mgupta1405 <mguptarocks@yahoo.co.in>
2017-07-30 21:44:09 +0530

    Added: Access properties using query
    1. set a property using Map object.
    2. delete single/all properties.
    3. access properties via query

--- data-driven/src/main/java/com/easyautomation/datadrivenmadeeasy/properties/AccessPropertiesFile.java
+++ data-driven/src/main/java/com/easyautomation/datadrivenmadeeasy/properties/AccessPropertiesFile.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.Properties;
 
 import com.easyautomation.datadrivenmadeeasy.exceptions.NoSuchPropertyException;
+import com.easyautomation.datadrivenmadeeasy.queryattributes.QueryFromPropertiesFile;
 
 public class AccessPropertiesFile {
 	
@@ -19,10 +20,12 @@ public class AccessPropertiesFile {
 	File file;
 	FileInputStream input;
 	FileOutputStream output;
+	QueryFromPropertiesFile q;
 	
 	public AccessPropertiesFile(File file) {
 		prop = new Properties();
 		this.file = file;
+		q = new QueryFromPropertiesFile();
 	}
 	
 	public String getAPropertyValue(String property) throws Exception {
@@ -151,8 +154,158 @@ public class AccessPropertiesFile {
 			}
 		}
 	}
-
-	// Select * from properties
-	// Select prop1,prop2 from properties
 	
+	public void setAProperty(Map<String,String> property) throws Exception {
+		try {
+			input = new FileInputStream(file);
+			prop.load(input);
+			for(Map.Entry<String, String> e : property.entrySet()) {
+				prop.setProperty(e.getKey(), e.getValue());
+			}
+			output = new FileOutputStream(file);
+			prop.store(output, "");
+		}
+		catch(FileNotFoundException e) {
+			throw new FileNotFoundException("File " + file + " is not found");
+		}
+		catch(IOException e) {
+			throw new IOException("Could not read file " + file);
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		finally {
+			if(input != null) { 
+				input.close();
+			}
+			if(output != null) { 
+				output.close();
+			}
+		}
+	}
+	
+	public void deleteAProperty(String property) throws Exception {
+		try {
+			input = new FileInputStream(file);
+			prop.load(input);
+			if(prop.containsKey(property)) {
+				prop.remove(property);
+			}
+			else {
+				throw new NoSuchPropertyException();
+			}
+			output = new FileOutputStream(file);
+			prop.store(output, "");
+		}
+		catch(NoSuchPropertyException e) {
+			throw new NoSuchPropertyException("Given property " + property + " is not present");
+		}
+		catch(FileNotFoundException e) {
+			throw new FileNotFoundException("File " + file + " is not found");
+		}
+		catch(IOException e) {
+			throw new IOException("Could not read file " + file);
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		finally {
+			if(input != null) { 
+				input.close();
+			}
+			if(output != null) { 
+				output.close();
+			}
+		}
+	}
+	
+	public void deleteAllProperties() throws Exception {
+		try {
+			input = new FileInputStream(file);
+			prop.load(input);
+			
+			Enumeration<?> enuKeys = prop.keys();
+			while (enuKeys.hasMoreElements()) {
+				String key = (String) enuKeys.nextElement();
+				prop.remove(key);
+			}
+			output = new FileOutputStream(file);
+			prop.store(output, "");
+		}
+		catch(FileNotFoundException e) {
+			throw new FileNotFoundException("File " + file + " is not found");
+		}
+		catch(IOException e) {
+			throw new IOException("Could not read file " + file);
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		finally {
+			if(input != null) {
+				input.close();
+			}
+			if(output != null) { 
+				output.close();
+			}
+		}
+	}
+	
+	public Map<String,String> executeQuery(String query) throws Exception {
+		String operation = q.getOperationFromQuery(query);
+		Map<String,String> properties = new HashMap<String,String>();
+		switch(operation) {
+			// Select *
+			// Select prop1,prop2
+			case "Select":
+			case "select":
+				ArrayList<String> parametersList = q.getAttributesFromSelectQuery(query);
+				if(parametersList.size() == 1) {
+					if(parametersList.get(0) == "*") {
+						properties = readAllProperties();
+					}
+					else {
+						String value = getAPropertyValue(parametersList.get(0));
+						properties.put(parametersList.get(0), value);
+					}
+				}
+				else {
+					properties = readProperties(parametersList);
+				}
+				break;
+			// Update prop=value
+			case "Update":
+			case "update":
+				Map<String,String> property_Update = q.getPropertyAndValueFromUpdateQuery(query);
+				setAProperty(property_Update);
+				break;
+			// Delete *
+			// Delete prop,prop2
+			case "Delete":
+			case "delete":
+				ArrayList<String> propertiesList = q.getAttributesFromDeleteQuery(query);
+				if(propertiesList.size() == 1) {
+					if(propertiesList.get(0) == "*") {
+						deleteAllProperties();
+					}
+					else {
+						deleteAProperty(propertiesList.get(0));
+					}
+				}
+				else {
+					for(String p : propertiesList) {
+						deleteAProperty(p);
+					}
+				}
+				break;
+			// Add Prop=value
+			case "Add":
+			case "add":
+				Map<String,String> property_Add = q.getPropertyAndValueFromAddQuery(query);
+				setAProperty(property_Add);
+				break;
+		}
+		
+		return properties;
+	}
 }
---
+++ data-driven/src/main/java/com/easyautomation/datadrivenmadeeasy/queryattributes/QueryFromPropertiesFile.java
@@ -0,0 +1,163 @@
+package com.easyautomation.datadrivenmadeeasy.queryattributes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.easyautomation.datadrivenmadeeasy.exceptions.InvalidOpertationException;
+import com.easyautomation.datadrivenmadeeasy.exceptions.InvalidQueryException;
+
+public class QueryFromPropertiesFile {
+	
+	public String getOperationFromQuery(String query) throws Exception {
+		String task = null;
+		
+		String[] queryText = query.split(" ");
+		try {
+			if(query.trim().startsWith("Select") || query.trim().startsWith("start")) {
+				task = queryText[0];
+			}
+			else if(query.trim().startsWith("Update") || query.trim().startsWith("update")) {
+				task = queryText[0];
+			}
+			else if(query.trim().startsWith("Delete") || query.trim().startsWith("delete")) {
+				task = queryText[0];
+			}
+			else if(query.trim().startsWith("Add") || query.trim().startsWith("add")) {
+				task = queryText[0];
+			}
+			else {
+				throw new InvalidOpertationException();
+			}
+		}
+		catch(InvalidOpertationException e) {
+			throw new InvalidOpertationException("Operation in query " + query + " is not valid");
+		}
+		catch (Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		
+		return task;
+	}
+	
+	public ArrayList<String> getAttributesFromSelectQuery(String query) throws Exception {
+		ArrayList<String> parametersList = new ArrayList<String>();
+		String[] queryParts = query.trim().split(" ");
+		try {
+			if(queryParts.length > 2 || queryParts.length < 2) {
+				switch(queryParts[1]) {
+					case "*":
+						parametersList.add("*");
+						break;
+					default:
+						String[] parameters;
+						if(queryParts[1].contains(",")){
+							parameters = queryParts[1].split(",");
+							for(String parameter : parameters) {
+								parametersList.add(parameter);
+							}
+						}
+						else {
+							parametersList.add(queryParts[1]);
+						}
+						break;
+				}
+			}
+			else {
+				throw new InvalidQueryException();
+			}
+		}
+		catch(InvalidQueryException e) {
+			throw new InvalidQueryException("Query " + query + " is not valid");
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		
+		return parametersList;
+	}
+	
+	public ArrayList<String> getAttributesFromDeleteQuery(String query) throws Exception {
+		ArrayList<String> parametersList = new ArrayList<String>();
+		String[] queryParts = query.trim().split(" ");
+		try {
+			if(queryParts.length > 2 || queryParts.length < 2) {
+				switch(queryParts[1]) {
+					case "*":
+						parametersList.add("*");
+						break;
+					default:
+						String[] parameters;
+						if(queryParts[1].contains(",")){
+							parameters = queryParts[1].split(",");
+							for(String parameter : parameters) {
+								parametersList.add(parameter);
+							}
+						}
+						else {
+							parametersList.add(queryParts[1]);
+						}
+						break;
+				}
+			}
+			else {
+				throw new InvalidQueryException();
+			}
+		}
+		catch(InvalidQueryException e) {
+			throw new InvalidQueryException("Query " + query + " is not valid");
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		
+		return parametersList;
+	}
+	
+	public Map<String,String> getPropertyAndValueFromUpdateQuery(String query) throws Exception {
+		Map<String,String> properties = new HashMap<String,String>();
+		
+		String[] queryParts = query.split(" ");
+		try {
+			if(queryParts.length == 2) {
+				String[] propertyValue = queryParts[1].trim().split("=");
+				properties.put(propertyValue[0], propertyValue[1]);
+			}
+			else {
+				throw new InvalidQueryException();
+			}
+		}
+		catch(InvalidQueryException e) {
+			throw new InvalidQueryException("Query " + query + " is not valid");
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		
+		return properties;
+	}
+	
+	public Map<String,String> getPropertyAndValueFromAddQuery(String query) throws Exception {
+		Map<String,String> properties = new HashMap<String,String>();
+		
+		String[] queryParts = query.split(" ");
+		try {
+			if(queryParts.length == 2) {
+				String[] propertyValue = queryParts[1].trim().split("=");
+				properties.put(propertyValue[0], propertyValue[1]);
+			}
+			else {
+				throw new InvalidQueryException();
+			}
+		}
+		catch(InvalidQueryException e) {
+			throw new InvalidQueryException("Query " + query + " is not valid");
+		}
+		catch(Exception e) {
+			throw new Exception(e.getCause().toString());
+		}
+		
+		return properties;
+	}
+	
+}
